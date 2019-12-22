d2e3ec25f098b1d82477a404099cedc3312ae808
League Student <league@iMac-14.attlocal.net>
2019-12-22 12:33:54 -0800

    yee

---
+++ src/_04_chuckle_clicker/cc.java
@@ -0,0 +1,48 @@
+package _04_chuckle_clicker;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+public class cc implements ActionListener {
+public static void main(String[] args) {
+	cc lll=new cc();
+	lll.makebutton();
+
+
+
+}
+
+
+
+
+private void makebutton() {
+	// TODO Auto-generated method stub
+	JOptionPane.showMessageDialog(null, "makebotton");
+	JFrame frame=new JFrame();
+frame.setVisible(true);
+JPanel panel=new JPanel();
+JButton button=new JButton();
+JButton button1=new JButton();
+panel.add(button);
+panel.add(button1);
+frame.add(panel);
+button.setText("jokeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+button1.setText("punchlineeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee");
+button.addActionListener(this);
+button1.addActionListener(this);
+}
+
+
+
+
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	
+}
+}
